0cea473cea0d7adc751b9c2fc7de6fdc4a5a2be9
MrFantan <aaronpla@gmail.com>
2023-07-22 22:43:36 +0200

    .

--- buscador.py
+++ buscador.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
 df = pd.read_excel("./Datos/almacen.xlsx")
-print(df)
+
+respuesta = input("quieres buscar o ingresar? (B/I):  ")
+
+buscar = "b"
+ingresar = "i"
+
+if respuesta != ingresar:
+    print(df)
+else:
+    respuesta != buscar
+    from almacen import ingresar_datos
