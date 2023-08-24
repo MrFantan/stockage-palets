63e52a054499b1897685bbad4280612cb1270fbd
MrFantan <aaronpla@gmail.com>
2023-08-24 13:52:17 +0200

    Añadido buscar por cliente

--- buscador.py
+++ buscador.py
@@ -2,14 +2,18 @@ import pandas as pd
 
 df = pd.read_excel("./Datos/almacen.xlsx")
 
-respuesta = input("quieres buscar o ingresar? (B/I):  ")
+respuesta = input("quieres buscar referencia, cliente o ingresar? (B/C/I):  ")
 
 buscar = "b"
 ingresar = "i"
+cliente = "c"
 
 if respuesta == ingresar:
     from almacen import ingresar_datos
     print("Datos insertados correctamente")
+if respuesta == cliente:
+    buscar = input("Que cliente busca: ")
+    print(df[df['Cliente'] == buscar])
 else:
     buscar = input("Que referencia busca: ")
     print(df[df['Referencia'] == buscar])
